570a195cd3a960767d6110892af926b66ea6ca18
Brenden Alves <your.email@example.com>
2025-10-10 09:13:10 -0400

    leanred about the difference between let const and var

---
+++ javascript_declaration.js
@@ -0,0 +1,24 @@
+//declare width and height of a rectangle. print the area to the console
+let width = 5;
+let height = 10;
+let area = width * height;
+console.log ("Area of the Rectangle is:" + area);
+//what is the different between var, let, and const 
+//Answer:
+//var lets you redeclare and update a variable 
+//let does not allow redeclaration 
+
+var a=10;
+function myName(){
+    a=20; 
+    return a;
+}
+myName();
+console.log(a);
+let b=10;
+function returnb(){
+    let b=20;
+    return b;
+}
+returnb();
+console.log(b);
